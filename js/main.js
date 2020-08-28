12a9bf976ed7ed7e5a335c4471fbc819c1748163
Hekk3 <timurh434@gmail.com>
2020-08-28 14:27:34 +0300

    konstuct

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// I took it from stackoverflow
+
+var btnScrollDown = document.querySelector('.scroll-down');
+
+function scrollDown() {
+    var windowCoords = document.documentElement.clientHeight;
+
+    (function scroll() {
+        if (Math.ceil(window.pageYOffset) < Math.ceil(windowCoords)) {
+            window.scrollBy(0, 10);
+            setTimeout(scroll, 2);
+        }
+            if (Math.ceil(window.pageYOffset) > Math.ceil(windowCoords)) {
+                window.scrollTo(0, Math.ceil(windowCoords));
+        }
+    })();
+};
+
+btnScrollDown.addEventListener('click', scrollDown);
